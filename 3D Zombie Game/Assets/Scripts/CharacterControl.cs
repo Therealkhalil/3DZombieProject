0b0592a668225e41f124519e84cbcb333f19b596
Therealkhalil <khalil.chabir96@gmail.com>
2023-05-07 18:37:25 +0100

    innit last project

---
+++ 3D Zombie Game/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    [SerializeField] private GameObject attackBox;
+    [SerializeField] private float attackDuration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(ActivateAttack());
+        }
+    }
+
+    //Activate Attack Function
+    public IEnumerator ActivateAttack()
+    {
+        attackBox.SetActive(true);
+        //wait .5 sec
+        yield return new WaitForSeconds(attackDuration);
+        attackBox.SetActive(false);
+    }
+}
